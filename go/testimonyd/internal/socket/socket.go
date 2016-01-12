e63703b501336a00c43758c1d31ae3b29986c285
Graeme Connell <gconnell@google.com>
2016-01-12 14:21:59 -0700

    Move socket.h contents into header of socket.go.

--- go/testimonyd/internal/socket/socket.go
+++ go/testimonyd/internal/socket/socket.go
@@ -14,10 +14,19 @@
 
 package socket
 
-// #include "socket.h"
-// #include <linux/if_packet.h>
-// #include <linux/filter.h>
-// #include <stdlib.h>  // for C.free
+/*
+#include <linux/if_packet.h>
+#include <linux/filter.h>
+#include <stdlib.h>  // for C.free
+
+struct sock_fprog;
+
+// See comments in socket.cc
+int AFPacket(const char* iface, int block_size, int block_nr, int block_ms,
+             int fanout_id, int fanout_type, const struct sock_fprog* filt,
+             // Outputs:
+             int* fd, void** ring, const char** err);
+*/
 import "C"
 
 import (
